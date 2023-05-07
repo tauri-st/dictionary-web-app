99e1fba0d2125003296061b6bdd691041ca190dd
tauri_s <tauristclaire@gmail.com>
2023-05-06 18:27:09 -0700

    Merged redundant classes in HTML and CSS referring to toggle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-var button = document.querySelector("#dark-mode");
+var themeSwitchInput = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
 var body = document.querySelector("body");
+var slider = document.querySelector(".switch");
 
-button.addEventListener("click", function () {
-    if (body.classList.contains("dark-mode-switch")) {
-        body.classList.remove("dark-mode-switch");
+themeSwitchInput.addEventListener("click", function () {
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        ball.classList.remove("move-right");
+        slider.classList.remove("switch-purple");
     } else {
-    body.classList.add("dark-mode-switch");
+    body.classList.add("dark");
+    ball.classList.add("move-right");
+    slider.classList.add("switch-purple");
     }
 });
