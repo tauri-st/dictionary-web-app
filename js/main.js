f4f69e7b30feb1b459d6570bda8371b4afa89941
tauri_s <tauristclaire@gmail.com>
2023-05-12 16:45:47 -0700

    Added purple properties for switch and moon

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const themeSwitchInput = document.querySelector("#toggle");
 const ball = document.querySelector(".ball");
 const body = document.querySelector("body");
+const moon = document.querySelector(".moon");
+const toggle = document.querySelector("switch");
 
 themeSwitchInput.addEventListener("click", function () {
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
         ball.classList.remove("move-right");
+        moon.classList.remove("purple");
+        toggle.classList.remove("purple");
     } else {
     body.classList.add("dark");
     ball.classList.add("move-right");
+    moon.classList.add("purple");
+    toggle.classList.add("purple");
     }
 });
